88bac8b83725f0685539917af18e8a951d096a8d
cavalcant3 <jmatheusoc269@gmail.com>
2021-07-13 10:06:51 -0300

    first commit

---
+++ page/src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <a>Learn React</a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
